32f55df2adff00fc3956a3d91c4fe7a7a7f215ef
d33pcode <mpanunzio@mail.com>
2016-10-21 00:40:25 +0200

    compressor: introducing new compression methods.
    Not using dirtools anymore, because it didn't provide methods for decompression.
    It seems I'll have to start writing all of the compression/decompression functions by myself - with the help of python's tarfile, of course.
    
    Archives will remain tar, but compression mode is now bz2:
    tar usually compresses better with bzip2 than with gzip.

--- agents/compressor.py
+++ agents/compressor.py
@@ -14,33 +14,43 @@ import os
 import sys
 from utils import questions
 import datetime
-from dirtools import Dir
+import tarfile
 
 def compress(path):
 	'''
-		Compresses input path to default backup folder
+		Stores a folder inside a tar archive compressed with bzip2
+		path:
+			the full path of the folder to compress
 	'''
-	try:
-		dir = Dir(path)
-	except TypeError:
-		sys.exit('You must select a valid directory')
-	archive_path = generate_archive_path(path)
+	archive_path = generate_archive_path(path=path)
 
 	if (os.path.exists(archive_path)):
 		replace = questions.queryYesNo("WARNING: you already did a backup today. Do you want to replace it?", default="no")
 		if not replace:
 			sys.exit('Backup canceled.')
-	return dir.compress_to(archive_path) # the result is the path of the compressed archive
+	print 'Creating the bzip2 archive...'
+	archive = tarfile.open(archive_path, mode='w:bz2') # tar usually compresses better with bzip2 than with gzip
+	with tarfile.open(archive_path, mode='w:bz2') as archive:
+		print 'Looping through subdirs...'
+		for subdir, dirs, files in os.walk(path):
+			for file in files:
+				archive.add(os.path.join(subdir, file))
+	return archive_path
 
-def generate_archive_path(path):
+def generate_archive_path(path, base_path='/var/backups/sbam/'):
 	'''
-		example:	/var/backups/sbam/d33pcode-Dev-2016-10-10
+		Generates a path for the archive.
+		path:
+			the original path
+		base_path:
+			the directory in which the archive will be stored
+		example:
+			/var/backups/sbam/Dev-2016-10-10
 	'''
-	base_path = '/var/backups/sbam/'
 	if (not os.path.exists(base_path)):
 		os.makedirs(base_path)
-
 	path_dirs = path.split('/')
 	dirname = path_dirs[len(path_dirs)-1]
 	time =  str(datetime.date.today())
-	return base_path + dirname + '-' + time + '.tar.gz'
+	full_path = base_path + dirname + '-' + time + '.tar.bz2'
+	return full_path
