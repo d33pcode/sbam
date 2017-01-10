f5e68a0f7a3bfe871916f77b95e22faaaa4fe1cb
d33pcode <mpanunzio@mail.com>
2017-01-10 11:05:27 +0100

    sbam: implemented verbosity - closed #3

--- agents/compressor.py
+++ agents/compressor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-	Handles folder compression with tar.
+Handles folder compression with tar.
 """
 
 __author__ = "d33pcode"
@@ -12,41 +12,45 @@ __date__ = "2016-10-10"
 
 import os
 import sys
-from utils import questions
+from utils import questions, parser
 import datetime
 import tarfile
+import logging
 
 def compress(path):
 	'''
-		Stores a folder inside a tar archive compressed with bzip2
-		path:
-			the full path of the folder to compress
+	Stores a folder inside a tar archive compressed with bzip2
+	path:
+		the full path of the folder to compress
 	'''
 	archive_path = generate_archive_path(path=path)
 
 	if (os.path.exists(archive_path)):
-		replace = questions.queryYesNo("WARNING: you already have a backup for this folder. Do you want to replace it?", default="no")
+		replace = questions.queryYesNo("WARNING: you already have today's backup for this folder. Do you want to replace it?", default="no")
 		if not replace:
-			sys.exit('Backup canceled.')
-	print 'Creating the bzip2 archive...'
-	archive = tarfile.open(archive_path, mode='w:bz2') # tar usually compresses better with bzip2 than with gzip
+			logging.info('Backup canceled.')
+			return None
+	logging.debug('Creating the bzip2 archive...')
+	archive = tarfile.open(archive_path, mode='w:bz2')
 	with tarfile.open(archive_path, mode='w:bz2') as archive:
-		print 'Looping through subdirs...'
+		logging.debug('Looping through subdirs...')
 		for subdir, dirs, files in os.walk(path):
-			for file in files:
-				archive.add(os.path.join(subdir, file))
+			for f in files:
+				logging.debug('Adding ' + os.path.join(subdir, f) + ' to the archive.')
+				archive.add(os.path.join(subdir, f))
 	return archive_path
 
 def generate_archive_path(path, base_path='/var/backups/sbam/'):
 	'''
-		Generates a path for the archive.
-		path:
-			the original path
-		base_path:
-			the directory in which the archive will be stored
-		example:
-			/var/backups/sbam/Dev-2016-10-10
+	Generates a path for the archive.
+	path:
+		the original path
+	base_path:
+		the directory in which the archive will be stored
+	example:
+		/var/backups/sbam/Dev-2016-10-10
 	'''
+	logging.debug('Geneating archive path...')
 	if (not os.path.exists(base_path)):
 		os.makedirs(base_path)
 	path_dirs = path.split('/')
