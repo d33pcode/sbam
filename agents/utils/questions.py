ca759746b7c8a2fadc3a94bc8421ff08340aa71b
d33pcode <mpanunzio@mail.com>
2017-01-31 23:34:05 +0100

    agents.utils.questions: added simple question method

--- agents/utils/questions.py
+++ agents/utils/questions.py
@@ -12,36 +12,48 @@ __date__ = "2016-10-12"
 
 import sys
 
-def queryYesNo(question, default="yes"):
-	"""Ask a yes/no question via raw_input() and return their answer.
+def askForInput(question):
+	"""
+	Ask user to answer a question.
 
 	question:
 		a string that is presented to the user.
-	default:
-		the presumed answer if the user just hits <Enter>.
-		It must be "yes" (the default), "no" or None (meaning
-		an answer is required of the user).
-
-	The "answer" return value is True for "yes" or False for "no".
 	"""
-	valid = {"yes": True, "y": True, "Y": True,
-			 "no": False, "n": False, "N": False}
-	if default is None:
-		prompt = " [y/n] "
-	elif default == "yes":
-		prompt = " [Y/n] "
-	elif default == "no":
-		prompt = " [y/N] "
-	else:
-		raise ValueError("invalid default answer: '%s'" % default)
+	sys.stdout.write(question + '\n')
+	answer = raw_input()
+	return answer
 
-	while True:
-		sys.stdout.write(question + prompt)
-		choice = raw_input().lower()
-		if default is not None and choice == '':
-			return valid[default]
-		elif choice in valid:
-			return valid[choice]
-		else:
-			sys.stdout.write("Please respond with 'yes' or 'no' "
-							 "(or 'y' or 'n').\n")
+def queryYesNo(question, default="yes"):
+    """
+    Ask a yes/no question via raw_input() and return their answer.
+
+    question:
+            a string that is presented to the user.
+    default:
+            the presumed answer if the user just hits <Enter>.
+            It must be "yes" (the default), "no" or None (meaning
+            an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "Y": True,
+             "no": False, "n": False, "N": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default in ["yes", 'y', 'Y']:
+        prompt = " [Y/n] "
+    elif default in ["no", 'n', 'N']:
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
