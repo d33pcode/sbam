288f9d599b0c157f436b845bb906159dbcae9cb8
d33pcode <mpanunzio@mail.com>
2017-02-28 17:21:36 +0100

    tests: finished test_backup function

--- tests/test_sbam.py
+++ tests/test_sbam.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python
+"""
+SBaM main testing module.
+"""
 import argparse
+import datetime
 import logging
 import os
 import sqlite3
 import sys
-from datetime import datetime
 from getpass import getpass
 
 from tabulate import tabulate
@@ -22,20 +26,37 @@ def test_list():
     except KeyError:
         config_dir = '/home/' + os.environ['USER'] + '/.config/sbam/'
     db_path = config_dir + 'sbam.db'
-    print db_path
+    # needs to be executed first, cause it cleans the db too
+    sbam_table = sbam.backup_table(5)
     conn = sqlite3.connect(db_path)
     cur = conn.cursor()
     select = "select file_path, original_path, backup_date from backups order by backup_date desc limit 5"
     backups = [e for e in cur.execute(select)]
     conn.close()
     headers = ['backup path', 'original path', 'date']
-    assert tabulate(backups, headers,
-                    tablefmt="fancy_grid") == sbam.backup_table(5)
+    test_table = tabulate(backups, headers, tablefmt="fancy_grid")
+    assert test_table == sbam_table
 
 
-#
-# def test_backup():
-#
+def test_verbose():
+    test_logger = logging.basicConfig(format='%(message)s', level=logging.INFO)
+    assert test_logger == sbam.verbosity(True)
+
+
+def test_backup():
+    p = 'tests/test_backup/'
+    if not os.path.isdir(p):
+        os.makedirs(p)
+    with open(p + 'test_file', 'w') as f:
+        f.write('Test ' + str(datetime.date.today()))
+    sbam_archive_path = sbam.backup(p, False, False)
+    test_archive_path = '/var/backups/sbam/test_backup-' + \
+        str(datetime.date.today()) + '.tar.bz2'
+    test_result = sbam_archive_path == test_archive_path
+    os.remove(p + 'test_file')
+    os.remove(sbam_archive_path)
+    os.removedirs(p)
+    assert test_result
 #
 # def test_restore():
 #
@@ -43,4 +64,3 @@ def test_list():
 # def test_encrypt():
 #
 #
-# def test_verbose():
